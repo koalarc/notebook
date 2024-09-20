a59a0d7eb849cc494681cab6c13af3334f6a2c06
mingxiyan <mingxiyan@pingduoduo.com>
2024-09-20 10:31:50 +0800

    初始化springboot项目

---
+++ notebook-console/src/main/java/com/rc/service/notebook/console/ConsoleContext.java
@@ -0,0 +1,12 @@
+package com.rc.service.notebook.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleContext {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleContext.class, args);
+    }
+}
